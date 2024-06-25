3483c63c19bd5333dcbb0580fa7e75088c8f4e18
Julian Krüger <116820352+projectuniverse@users.noreply.github.com>
2024-06-25 13:32:33 +0200

    build: add dependencies for Preferences DataStore Hilt and ViewModel utilities

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -66,6 +68,13 @@ dependencies {
     implementation(libs.androidx.compose.material.material.icons.extended)
     // Navigation
     implementation(libs.androidx.navigation.compose)
+    // Preferences DataStore (to toggle dark mode/light mode)
+    implementation(libs.androidx.datastore.preferences)
+    // Hilt (dependency injection)
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-android-compiler:2.51.1")
+    // To use viewModel() for dependency injection
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -73,4 +82,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
 }
